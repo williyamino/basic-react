b3dc4dea970b03717ecd49eeeb0d89639df05bd7
williyamino <vasylyshyn1984@gmail.com>
2020-02-20 23:38:26 +0200

    added parcel and script to package json

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const Pet = ({ name, animal }) => {
 const App = () => {
   return React.createElement("div", { id: "important-id" }, [
     React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, { name: "Luna", animal: "dog" }),
+    React.createElement(Pet, { name: "Lunar", animal: "dog" }),
     React.createElement(Pet, { name: "Pepper", animal: "bird" }),
     React.createElement(Pet, { name: "Moon", animal: "cat" })
   ]);

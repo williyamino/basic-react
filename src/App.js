642f32cae944e61c6dcac1960d45c7eadfadc66f
williyamino <vasylyshyn1984@gmail.com>
2020-02-28 00:07:10 +0200

    added new  component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { render } from "react-dom";
 import { Pet } from "./Pet";
-
+import SearchParams from "./SearchParams";
 const App = () => {
   return (
     <div>
       <h1 id="important-id">Adopt Me!</h1>
-      <Pet name="Lunar" animal="dog" />
+      <SearchParams />
+      {/* <Pet name="Lunar" animal="dog" />
       <Pet name="Pepper" animal="bird" />
-      <Pet name="Moon" animal="cat" />
+      <Pet name="Moon" animal="cat" /> */}
     </div>
   );
 };

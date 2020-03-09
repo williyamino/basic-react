385027fb0a0f5a60ff1ba2534fdb20acb21ffb10
williyamino <vasylyshyn1984@gmail.com>
2020-03-10 00:33:39 +0200

    added get data from api

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,12 +1,22 @@
 import React from "react";
-import { ANIMALS } from "@frontendmasters/pet";
+import pet, { ANIMALS } from "@frontendmasters/pet";
 import useDropdown from "./useDropdown";
 
 const SearchParams = () => {
   const [location, setLocation] = React.useState('"Seatle, WA"');
   const [breeds, setBreeds] = React.useState([]);
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
-  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
+  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+
+  React.useEffect(() => {
+    setBreeds([]);
+    setBreed("");
+
+    pet.breeds(animal).then(({ breeds }) => {
+      const breedStrings = breeds.map(({ name }) => name);
+      setBreeds(breedStrings);
+    }, console.error);
+  }, [animal, setBreed, setBreeds]);
 
   return (
     <div className="search-params">
